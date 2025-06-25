4e7d754c33109b2d1699ab99d58445ce20b567d3
shankar3057 <shankar.kumar@appinventiv.com>
2025-06-25 15:39:47 +0530

    next config update

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,20 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
 
+  async headers() {
+    return [
+      {
+        source: '/.well-known/apple-developer-merchantid-domain-association',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'text/plain; charset=utf-8',
+          },
+        ],
+      },
+    ];
+  },
+
   compilerOptions: {
     typeRoots: ['./types', './node_modules/@types'],
   },
